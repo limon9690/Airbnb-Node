c5888c528f202aebabd261b7982547fd7b8f534e
limon9690 <limon.hossain26@yahoo.com>
2026-06-19 09:07:45 +0800

    added booking and idempotency_key table

--- BookingService/src/utils/lib/prisma.ts
+++ BookingService/src/utils/lib/prisma.ts
@@ -1,17 +1,17 @@
-// import { PrismaMariaDb } from "@prisma/adapter-mariadb";
-// import { PrismaClient } from "../../../generated/prisma/client";
-// import { serverConfig } from "../../config";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+import { PrismaClient } from "../../../generated/prisma/client";
+import { serverConfig } from "../../config";
 
-// const adapter = new PrismaMariaDb({
-//   host: serverConfig.DATABASE_HOST,
-//   port: serverConfig.DATABASE_PORT,
-//   user: serverConfig.DATABASE_USER,
-//   password: serverConfig.DATABASE_PASSWORD,
-//   database: serverConfig.DATABASE_NAME,
-//   connectionLimit: serverConfig.connectionLimit,
-//   allowPublicKeyRetrieval: true,
-// });
+const adapter = new PrismaMariaDb({
+  host: serverConfig.DATABASE_HOST,
+  port: serverConfig.DATABASE_PORT,
+  user: serverConfig.DATABASE_USER,
+  password: serverConfig.DATABASE_PASSWORD,
+  database: serverConfig.DATABASE_NAME,
+  connectionLimit: serverConfig.connectionLimit,
+  allowPublicKeyRetrieval: true,
+});
 
-// const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient({ adapter });
 
-// export { prisma };
+export { prisma };
