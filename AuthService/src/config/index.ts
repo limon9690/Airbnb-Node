f3b2c90e4f3819beedb8d03da6db7415c914f51a
limon9690 <limon.hossain26@yahoo.com>
2026-08-03 00:46:15 +0800

    added db config and models

--- AuthService/src/config/index.ts
+++ AuthService/src/config/index.ts
@@ -1,17 +1,31 @@
 // This file contains all the basic configuration logic for the app server to work
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 type ServerConfig = {
-    PORT: number
-}
+  PORT: number;
+  DATABASE_URL: string;
+  DATABASE_HOST: string;
+  DATABASE_PORT: number;
+  DATABASE_USER: string;
+  DATABASE_PASSWORD: string;
+  DATABASE_NAME: string;
+  connectionLimit: number;
+};
 
 function loadEnv() {
-    dotenv.config();
-    console.log(`Environment variables loaded`);
+  dotenv.config();
+  console.log(`Environment variables loaded`);
 }
 
 loadEnv();
 
 export const serverConfig: ServerConfig = {
-    PORT: Number(process.env.PORT) || 3001
+  PORT: Number(process.env.PORT) || 3001,
+  DATABASE_URL: process.env.DATABASE_URL || "",
+  DATABASE_HOST: process.env.DATABASE_HOST || "localhost",
+  DATABASE_PORT: Number(process.env.DATABASE_PORT) || 3306,
+  DATABASE_USER: process.env.DATABASE_USER || "root",
+  DATABASE_PASSWORD: process.env.DATABASE_PASSWORD || "root",
+  DATABASE_NAME: process.env.DATABASE_NAME || "airbnb_dev_booking",
+  connectionLimit: Number(process.env.connectionLimit) || 5,
 };
